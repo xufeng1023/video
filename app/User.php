d8f9ac8d219d699bb96270547d13434fb81941d1
Xu Feng <xufeng1023@hotmail.com>
2017-09-14 17:48:55 -0700

    'working-on-payment'

--- app/User.php
+++ app/User.php
@@ -3,12 +3,13 @@
 namespace App;
 
 use Carbon\Carbon;
+use Laravel\Cashier\Billable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, Billable;
 
     protected $fillable = [
         'name', 'email', 'password',
